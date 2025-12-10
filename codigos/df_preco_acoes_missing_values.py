5425358aadea586cbec7779143eb25ee98372e43
sanderpiva <84524010+sanderpiva@users.noreply.github.com>
2025-12-10 13:50:07 -0300

    Update comment section for loading stock prices dataframe

--- codigos/df_preco_acoes_missing_values.py
+++ codigos/df_preco_acoes_missing_values.py
@@ -1,5 +1,6 @@
-# --- 4 Carregando dataframe de preços de ações e verificando existência de valores missing ---
+#7.1 Carregando dataframe de preços de ações e verificando existência de valores missing
 df_preco = pd.read_csv('/content/precos_acoes_5anos.csv')
 display(df_preco.head())
 df_preco.info()
+
 
